243f87d7db225efaf0d0e18409490de7cc350559
Jeffrey Ullers <jeffrey.ullers@hva.nl>
2022-03-17 15:55:30 +0100

    finished intro

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,7 @@ const audiobook = `&facet=Type(audiobook)`
 const film = `&facet=Type(movie)`
 const schooltv = `&facet=Type(schooltv)`
 const uitreksel = `&facet=Type(excerpt)`
+const activities = `&facet=activiteiten(g_thisyear)`
 
 
 let topic = `&facet=Topic(Ondernemerschap)`
@@ -62,18 +63,25 @@ let introIndex = 1; // start positie
 
 const introPrevious = document.querySelector(`section div button:first-child`)
 const introNext = document.querySelector(`section div button:last-child`)
+const introSection = document.querySelector(`section[title="Introductie ondernemen"]`)
+const optionSection = document.querySelector(`  section[title="onderwerpen selecteren"]`)
+
 introPrevious.style.color = `rgba(205 205 205 / 90%)`
 document.querySelector(`section div ul li:first-child span`).style.backgroundColor = `red`
 
 introNext.addEventListener("click", function(){
     if(introIndex == 5){
         // verderbutton disabled maken
+        introSection.style.setProperty('display', 'none')
+        optionSection.style.setProperty('display', 'flex')
       }else{
-        document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.display = `none`
+        // document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.display = `none`
+        document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.setProperty('display', 'none') 
         introIndex++
-        document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.display = `flex`
-        introPrevious.style.color = `black`
-        document.querySelector(`section div ul li:nth-child(${introIndex}) span`).style.backgroundColor = `red`
+        // document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.display = `flex`
+        document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.setProperty('display','flex')
+        introPrevious.style.setProperty('color','black')
+        document.querySelector(`section div ul li:nth-child(${introIndex}) span`).style.setProperty('background-color' , 'red')
       }
       checkNeedLoad();
 })
@@ -81,17 +89,18 @@ introNext.addEventListener("click", function(){
 introPrevious.addEventListener("click", function(){
     if(introIndex == 1){
         // terugknop disabled maken
-        introPrevious.style.color = `rgba(205 205 205 / 90%)`
-        document.querySelector(`section div ul li:first-child span`).style.backgroundColor = `red`
+        introPrevious.style.setProperty('color','rgba(205 205 205 / 90%)')
+        // document.querySelector(`section div ul li:first-child span`).style.backgroundColor = `red`
+        document.querySelector(`section div ul li:first-child span`).style.setProperty('background-color' , 'red')
       }else{
-        document.querySelector(`section div ul li:nth-child(${introIndex}) span`).style.backgroundColor = `white`
-        document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.display = `none`
+        document.querySelector(`section div ul li:nth-child(${introIndex}) span`).style.setProperty('background-color' , 'white')
+        document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.setProperty('display', 'none') 
         introIndex--
-        document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.display = `flex`
+        document.querySelector(`section>ul>li:nth-child(${introIndex})`).style.setProperty('display','flex')
 
         if(introIndex == 1){
             introPrevious.style.color = `rgba(205 205 205 / 90%)`
-            document.querySelector(`section div ul li:first-child span`).style.backgroundColor = `red`
+            document.querySelector(`section div ul li:first-child span`).style.setProperty('background-color' , 'red')
         }
     }
     checkNeedLoad();
@@ -113,10 +122,14 @@ function checkNeedLoad(){
             renderAudioBooks()
         break;
         case 4:
+            url = `${corsURL}${endpoint}${query}${film}${refine}&authorization=${pubKey}&output=json`;
             console.log('case 4')
+            renderMovies()
         break;
         case 5:
-            console.log('case 5')
+            url = `${corsURL}${endpoint}${query}${activities}${refine}&authorization=${pubKey}&output=json`;
+            console.log('case 5', url)
+            renderActivities()
         break;
     }
 }
@@ -157,6 +170,29 @@ async function renderAudioBooks(){
     }
 }
 
+async function renderMovies(){
+    let films = await getData();
+    console.log(films)
+
+    let i
+    for(i = 0; i < 7; i++){
+        let movies = document.querySelector(`section[title="Introductie ondernemen"]>ul>li:nth-child(4) .container>.book-${i}`)
+        movies.innerHTML = ''
+        movies.insertAdjacentHTML('beforeend',`<img src="${films.results[i].coverimages[1]}" alt="">`)
+    }
+}
+
+async function renderActivities(){
+    let activities = await getData()
+    console.log(activities)
+
+    let i
+    for(i = 1; i < 3; i++){ // als ik bij 0 begin, dan vraagt ie om nth-child(0)
+        let listItem = document.querySelector(`section[title="Introductie ondernemen"]>ul>li:nth-child(5) ul li:nth-child(${i})`)
+        listItem.innerHTML = ''
+        listItem.insertAdjacentHTML('beforeend', `<img src="${activities.results[i-1].coverimages[0]}" alt=""><h3>${activities.results[i-1].titles[0]}</h3>`)
+    }
+}
 // **** Eind intro **** //
 
 async function getError(){
